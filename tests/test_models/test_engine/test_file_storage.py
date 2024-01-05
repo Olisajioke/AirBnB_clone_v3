5e6be552932dbac4f8072304163f869a08756fb7
Olisajioke <chijioke914@gmail.com>
2024-01-05 12:50:53 -0800

    updated dbstorage and filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,26 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    def test_get(self):
+        """ Tests that evaluate method for obtainig an instance db storage"""
+        storage = DBStorage()
+        dbs = {"name": "Alxgracebright"}
+        instance = State(**dbs)
+        storage.new(instance)
+        storage.save()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
+
+    def test_count(self):
+        """ Tests thar evaluate count method bd storage"""
+        storage = DBStorage()
+        dbs = {"name": "Alxgracebright"}
+        state = State(**dbs)
+        storage.new(state)
+        dbs = {"name": "Mexico"}
+        city = City(**dbs)
+        storage.new(city)
+        storage.save()
+        count = storage.count()
+        self.assertEqual(len(storage.all()), count)
