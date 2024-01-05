7af256c41d01ef293eeba0f5799501b3faaf7997
Olisajioke <chijioke914@gmail.com>
2024-01-05 13:51:56 -0800

    updated

---
+++ api/v1/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Creates Flask ap to register the blueprint app_views Flask instance app.'''
+
+from os import getenv
+from flask import Flask, jsonify
+# from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    ''' function that removes the current SQLAl Session obj after each req'''
+    storage.close()
+
+
+# Error handlers for expected app behavior:
+@app.errorhandler(404)
+def not_found(error):
+    ''' function that Returns errmsg `Not Found '''
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Script that creates a route/ status on the object app_views"""
+
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    """Function that returns JSON response for RESTful API"""
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """ Function that retrieves the number of each objects due to type"""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
